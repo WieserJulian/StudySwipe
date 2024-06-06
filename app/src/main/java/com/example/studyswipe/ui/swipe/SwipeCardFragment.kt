d084c93cbbbb75b8bc3f245fd1760be09f6686da
WieserJulian-Yggdrasil <julian.wieser@i-wieser.com>
2024-06-06 16:48:41 +0200

    It fucking swipes mate

--- app/src/main/java/com/example/studyswipe/ui/swipe/SwipeCardFragment.kt
+++ app/src/main/java/com/example/studyswipe/ui/swipe/SwipeCardFragment.kt
@@ -1,7 +1,11 @@
 package com.example.studyswipe.ui.swipe
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import android.view.GestureDetector
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,16 +15,22 @@ import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.core.view.GestureDetectorCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studyswipe.R
 import com.example.studyswipe.databinding.FragmentHomeBinding
 import com.example.studyswipe.databinding.FragmentSwipeCardBinding
 import com.example.studyswipe.ui.home.HomeViewModel
+import com.example.studyswipe.util.Constants
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.min
 
 class SwipeCardFragment : Fragment() {
     private lateinit var cardView: CardView
-    private lateinit var gestureDetector: GestureDetectorCompat
+    private var hasMoved: Boolean = false
 
     private var _binding: FragmentSwipeCardBinding? = null
 
@@ -40,36 +50,47 @@ class SwipeCardFragment : Fragment() {
         return root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         cardView = view.findViewById(R.id.card_view)
 
-        gestureDetector = GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener() {
-            override fun onFling(
-                e1: MotionEvent?,
-                e2: MotionEvent,
-                velocityX: Float,
-                velocityY: Float
-            ): Boolean {
-                val diffX = e2.x - (e1?.x!! ?: 0f)
-                if (Math.abs(diffX) > Math.abs(e2.y - e1.y)) {
-                    if (Math.abs(diffX) > 100 && Math.abs(velocityX) > 100) {
-                        if (diffX > 0) {
-                            onSwipeRight()
-                        } else {
-                            onSwipeLeft()
-                        }
-                        return true
-                    }
-                }
-                return false
-            }
-        })
+        cardView.setOnTouchListener(
+            View.OnTouchListener { v, event ->
 
-        cardView.setOnTouchListener { _, event ->
-            gestureDetector.onTouchEvent(event)
-        }
+                // variables to store current configuration of quote card.
+                val displayMetrics = resources.displayMetrics
+                val cardWidth = cardView.width
+                val cardStart = (displayMetrics.widthPixels.toFloat() / 2) - (cardWidth / 2)
+
+                when (event.action) {
+                    MotionEvent.ACTION_MOVE -> {
+                        // get the new co-ordinate of X-axis
+                        val newX = event.rawX - (cardWidth / 2)
+                        cardView.animate()
+                            .x(newX)
+                            .setDuration(0)
+                            .start()
+                        if (cardView.x < -Constants.MIN_SWIPE_DISTANCE && !hasMoved) {
+                            Log.d("SwipeCardFragment", "Swiped left")
+                            hasMoved = true
+                        } else if (cardView.x  - cardStart > Constants.MIN_SWIPE_DISTANCE && !hasMoved) {
+                            Log.d("SwipeCardFragment", "Swiped right")
+                            hasMoved = true
+                        }
+
+                    }
+
+                    MotionEvent.ACTION_UP ->  {
+                        hasMoved = false
+                        cardView.animate().x(cardStart).start()
+                    }
+
+                }
+                v.performClick()
+                return@OnTouchListener true
+            })
     }
 
     private fun onSwipeRight() {
---
+++ app/src/main/java/com/example/studyswipe/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.studyswipe.util
+
+class Constants {
+    companion object {
+        const val MIN_SWIPE_DISTANCE = 150
+    }
+}
