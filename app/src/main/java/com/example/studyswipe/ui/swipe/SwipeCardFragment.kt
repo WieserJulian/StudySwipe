13d5076c6736e33480d8675af91ed14c19a2e712
WieserJulian <julian.wieser@i-wieser.coom>
2024-06-07 21:08:19 +0200

    Swipeactivity

--- app/src/main/java/com/example/studyswipe/ui/swipe/SwipeCardFragment.kt
+++ app/src/main/java/com/example/studyswipe/ui/swipe/SwipeCardFragment.kt
@@ -2,51 +2,61 @@ package com.example.studyswipe.ui.swipe
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.util.Log
-import android.view.GestureDetector
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.cardview.widget.CardView
-import androidx.core.view.GestureDetectorCompat
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.fragment.findNavController
 import com.example.studyswipe.R
-import com.example.studyswipe.databinding.FragmentHomeBinding
 import com.example.studyswipe.databinding.FragmentSwipeCardBinding
 import com.example.studyswipe.ui.home.HomeViewModel
 import com.example.studyswipe.util.Constants
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.math.min
 
 class SwipeCardFragment : Fragment() {
     private lateinit var cardView: CardView
     private var hasMoved: Boolean = false
+    private var hasFlipped: Boolean = false
+    private var loadNextQuestion: Boolean = false
+    private lateinit var swipeCardViewModel: SwipeCardViewModel;
+    private var activeQuestion: Pair<String, String> = Pair("", "")
+    private var state: String = ""
 
     private var _binding: FragmentSwipeCardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentSwipeCardBinding.inflate(inflater, container, false)
+
+        swipeCardViewModel =
+            ViewModelProvider(this)[SwipeCardViewModel::class.java]
+        var homeViewModel =
+            ViewModelProvider(this)[HomeViewModel::class.java]
+
         val root: View = binding.root
+        val topicName = arguments?.getString("topic") ?: ""
+        Log.d("SwipeCardFragment", "Topic name: $topicName")
+        swipeCardViewModel.setAllQuestion(homeViewModel.getQuestion(topicName ?: ""))
 
-        binding.topicName.text = arguments?.getString("topicName")
-
+        binding.topicName.text = topicName
+        binding.swapImage.animate().alpha(1f).start()
+        binding.questionPoints.animate().alpha(0f).start()
+        activeQuestion = swipeCardViewModel.getNextQuestion()
+        binding.questionText.text = activeQuestion.first
+        binding.questionPoints.text = (0..10).random().toString() //TODO: Change to actual points
         return root
     }
 
@@ -56,9 +66,18 @@ class SwipeCardFragment : Fragment() {
 
         cardView = view.findViewById(R.id.card_view)
 
+        cardView.setOnClickListener {
+            if (!hasFlipped) {
+                hasFlipped = true
+                flipCard(activeQuestion.second)
+            }
+        }
+
         cardView.setOnTouchListener(
             View.OnTouchListener { v, event ->
-
+                if (!hasFlipped) {
+                    return@OnTouchListener false
+                }
                 // variables to store current configuration of quote card.
                 val displayMetrics = resources.displayMetrics
                 val cardWidth = cardView.width
@@ -72,19 +91,39 @@ class SwipeCardFragment : Fragment() {
                             .x(newX)
                             .setDuration(0)
                             .start()
-                        if (cardView.x < -Constants.MIN_SWIPE_DISTANCE && !hasMoved) {
+                        if (cardView.x < -Constants.MIN_SWIPE_DISTANCE && !hasMoved && state != "positive") {
                             Log.d("SwipeCardFragment", "Swiped left")
-                            hasMoved = true
-                        } else if (cardView.x  - cardStart > Constants.MIN_SWIPE_DISTANCE && !hasMoved) {
+                            state = "positive"
+
+                        } else if (cardView.x  - cardStart > Constants.MIN_SWIPE_DISTANCE && state != "negative") {
                             Log.d("SwipeCardFragment", "Swiped right")
-                            hasMoved = true
+                            state = "negative"
                         }
 
                     }
 
                     MotionEvent.ACTION_UP ->  {
                         hasMoved = false
-                        cardView.animate().x(cardStart).start()
+                        if (!swipeCardViewModel.hasNewQuestion() && state != "")  {
+                            cardView.animate().alpha(0f).start()
+                            safeAwnser(state)
+                            Log.d("SwipeCardFragment", "No more questions")
+                            val allQuestion = swipeCardViewModel.getAllQuestions()
+                            Log.d("SwipeCardFragment", "Positive: ${allQuestion["positive"]}")
+                            Log.d("SwipeCardFragment", "Negative: ${allQuestion["negative"]}")
+                            Log.d("SwipeCardFragment", "Retry: ${allQuestion["retry"]}")
+                            // TODO Exit this fragment to goal state
+                            findNavController().navigate(R.id.navigation_home)
+                        } else {
+                            cardView.animate()
+                                .x(cardStart)
+                                .setDuration(20)
+                                .start()
+                            if (state != "") {
+                                safeAwnser(state)
+                                loadNextQuestion()
+                            }
+                        }
                     }
 
                 }
@@ -93,13 +132,55 @@ class SwipeCardFragment : Fragment() {
             })
     }
 
-    private fun onSwipeRight() {
-        // Handle right swipe
-        cardView.animate().translationX(cardView.width.toFloat()).setDuration(200).start()
+    private fun flipCorner() {
+        val alS = if (binding.swapImage.alpha == 0f) 1f else 0f
+        val alQ = if (binding.questionPoints.alpha == 0f) 1f else 0f
+        binding.swapImage.alpha = alS
+        binding.questionPoints.alpha = alQ
     }
 
-    private fun onSwipeLeft() {
-        // Handle left swipe
-        cardView.animate().translationX(-cardView.width.toFloat()).setDuration(200).start()
+    private fun safeAwnser(state: String) {
+        when (state) {
+            "positive" -> swipeCardViewModel.addToPositive(activeQuestion)
+            "negative" -> swipeCardViewModel.addToNegative(activeQuestion)
+        }
+    }
+
+    private fun loadNextQuestion() {
+
+        if (!swipeCardViewModel.hasNewQuestion()) {
+            return
+        }
+        state = ""
+        activeQuestion = swipeCardViewModel.getNextQuestion()
+        binding.swapImage.alpha = 1f
+        binding.questionPoints.alpha = 0f
+        binding.questionText.text = activeQuestion.first
+        binding.questionPoints.text = (0..10).random().toString() //TODO: Change to actual points
+        cardView.alpha = 0f // Set initial alpha to 0
+        cardView.animate().alpha(1f).setDuration(100).withEndAction {
+            hasFlipped = false
+        }.start()
+    }
+
+
+    private fun flipCard(text: String) {
+        val oa1 = ObjectAnimator.ofFloat(cardView, "scaleX", 1f, 0f)
+        val oa2 = ObjectAnimator.ofFloat(cardView, "scaleX", 0f, 1f)
+
+        oa1.interpolator = AccelerateDecelerateInterpolator()
+        oa2.interpolator = AccelerateDecelerateInterpolator()
+
+        oa1.duration = 125
+        oa2.duration = 125
+
+        oa1.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                flipCorner()
+                binding.questionText.text = text
+                oa2.start()
+            }
+        })
+        oa1.start()
     }
 }
